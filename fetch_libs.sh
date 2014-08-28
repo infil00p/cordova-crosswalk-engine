e896fcffdf06bb056fced38c437b5b1dd7a5a185
Ian Clelland <iclelland@chromium.org>
2014-08-28 09:41:09 -0400

    Update fetch_libs.sh to get Crosswalk 8.37.189.0

--- fetch_libs.sh
+++ fetch_libs.sh
@@ -1,5 +1,5 @@
-ARM_DOWNLOAD="https://download.01.org/crosswalk/releases/crosswalk/android/canary/8.37.188.0/arm/crosswalk-webview-8.37.188.0-arm.zip";
-X86_DOWNLOAD="https://download.01.org/crosswalk/releases/crosswalk/android/canary/8.37.188.0/x86/crosswalk-webview-8.37.188.0-x86.zip";
+ARM_DOWNLOAD="https://download.01.org/crosswalk/releases/crosswalk/android/canary/8.37.189.0/arm/crosswalk-webview-8.37.189.0-arm.zip";
+X86_DOWNLOAD="https://download.01.org/crosswalk/releases/crosswalk/android/canary/8.37.189.0/x86/crosswalk-webview-8.37.189.0-x86.zip";
 
 mkdir -p libs/xwalk_core_library
 
