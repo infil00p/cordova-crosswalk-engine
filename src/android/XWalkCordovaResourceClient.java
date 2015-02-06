3ce1d7d854cf4e0dd0e1e17ac88daf7f3e9c3253
Andrew Grieve <agrieve@chromium.org>
2015-02-06 14:12:00 -0500

    Remove system webview-specific work-arounds in shouldInterceptLoadRequest

--- src/android/XWalkCordovaResourceClient.java
+++ src/android/XWalkCordovaResourceClient.java
@@ -223,7 +223,7 @@ public class XWalkCordovaResourceClient extends XWalkResourceClient {
             // Allow plugins to intercept WebView requests.
             Uri remappedUri = resourceApi.remapUri(origUri);
 
-            if (!origUri.equals(remappedUri) || needsSpecialsInAssetUrlFix(origUri) || needsKitKatContentUrlFix(origUri)) {
+            if (!origUri.equals(remappedUri)) {
                 OpenForReadResult result = resourceApi.openForRead(remappedUri, true);
                 return new WebResourceResponse(result.mimeType, "UTF-8", result.inputStream);
             }
@@ -242,29 +242,4 @@ public class XWalkCordovaResourceClient extends XWalkResourceClient {
     public boolean shouldOverrideUrlLoading(XWalkView view, String url) {
         return helper.shouldOverrideUrlLoading(url);
     }
-
-    private static boolean needsKitKatContentUrlFix(Uri uri) {
-        return android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT && "content".equals(uri.getScheme());
-    }
-
-    private static boolean needsSpecialsInAssetUrlFix(Uri uri) {
-        if (CordovaResourceApi.getUriType(uri) != CordovaResourceApi.URI_TYPE_ASSET) {
-            return false;
-        }
-        if (uri.getQuery() != null || uri.getFragment() != null) {
-            return true;
-        }
-
-        if (!uri.toString().contains("%")) {
-            return false;
-        }
-
-        switch(android.os.Build.VERSION.SDK_INT){
-            case android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH:
-            case android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1:
-                return true;
-        }
-        return false;
-    }
-
 }
