1f37e01e2a0812150d709a390b216f03a8886947
Andrew Grieve <agrieve@chromium.org>
2015-03-13 11:33:21 -0400

    When both allow-navigation and allow-external are set, navigate instead of opening external

--- src/android/XWalkCordovaResourceClient.java
+++ src/android/XWalkCordovaResourceClient.java
@@ -226,18 +226,6 @@ public class XWalkCordovaResourceClient extends XWalkResourceClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(XWalkView view, String url) {
-        // Give plugins the chance to handle the url
-        if (parentEngine.pluginManager.onOverrideUrlLoading(url)) {
-            return true;
-        } else if (parentEngine.pluginManager.shouldOpenExternalUrl(url)) {
-            parentEngine.getCordovaWebView().showWebPage(url, true, false, null);
-            return true;
-        } else if (!parentEngine.pluginManager.shouldAllowNavigation(url)) {
-            // This blocks iframe navigations as well.
-            LOG.w(TAG, "Blocked (possibly sub-frame) navigation to non-allowed URL: " + url);
-            return true;
-        }
-
-        return false;
+        return parentEngine.client.onNavigationAttempt(url);
     }
 }
